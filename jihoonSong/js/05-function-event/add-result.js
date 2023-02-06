90f564b3d8a7fafa8e98074af11fe21041a36ea5
jihoon.song <jihoon.song@medisolution.co.kr>
2023-02-06 18:26:50 +0900

    05 문제풀이 및 경로 수정중

---
+++ jihoonSong/js/05-function-event/add-result.js
@@ -0,0 +1,9 @@
+function addNumber() { 			
+	var sum = 0;
+	var no1 = prompt("first number", "숫자만 입력하세요");
+	var no2 = prompt("second number", "숫자만 입력하세요");
+
+	sum = Number(no1) + Number(no2);
+
+	alert(sum);
+}
