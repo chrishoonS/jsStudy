3b7237e501ba97b7bdbb67dbd978ff7447960b6a
jihoon.song <jihoon.song@medisolution.co.kr>
2023-02-09 11:35:13 +0900

    ch07 문제풀이 완료

--- jihoonSong/js/07-array-object/checklist-result.js
+++ jihoonSong/js/07-array-object/checklist-result.js
@@ -1,14 +1,28 @@
 		var itemList = [];
 		
-		var addBtn = document.querySelector("#add");
+		var addBtn = document.getElementById("add");
 		addBtn.addEventListener("click", addList); // addBtn.onclick = addList; 라고 해도 됨
+		//지정요소에 이벤트 핸들러 부착
+		/**
+		 * 참고: https://homzzang.com/b/js-67
+		 * 기존 이벤트는 그대로 둔 상태에서, 요소에 새 이벤트 부착.
+		 * 하나의 요소에 많은 이벤트 핸들러 부착 가능.
+		 * 하나의 요소에 동일 이벤트 여러 개 부착 가능.
+		 * HTML 요소뿐만 아니라 window 객체 등 모든 DOM 객체에 부착 가능.
+		 * 이벤트 버블링 (= 전이 = 확산)을 보다 쉽게 제어 가능.
+		 * 가독성을 높이기 위해 HTML 태그와 분리되어 있으며, HTML 태그 제어 안 해도 추가 가능.
+		 * removeEventListener() 메서드 사용해 쉽게 제거 가능.
+		 * jQuery 경우, on() 메서드 이용. 
+		 **/
+		
+
 		
 		function addList() {
-			var item = document.querySelector("#item").value;  // 텍스트 필드 내용 가져옴
+			var item = document.getElementById("item").value;  // 텍스트 필드 내용 가져옴
 			if (item != null) {
 				itemList.push(item);  // itemList 배열의 끝에 item 변수 값 추가
-				document.querySelector("#item").value = "";  // id=”item”인 요소의 값을 지움
-				document.querySelector("#item").focus();  // 텍스트 필드에 focus( ) 메서드 적용
+				document.getElementById("item").value = "";  // id=”item”인 요소의 값을 지움
+				document.getElementById("item").focus();  // 텍스트 필드에 focus( ) 메서드 적용
 			}
 			showList();
 		}
@@ -20,7 +34,7 @@
 			}
 			list += "</ul>";   // 목록을 끝내는 </ul> 태그 저장
 			
-			document.querySelector('#itemList').innerHTML = list;  // list 내용 표시
+			document.getElementById("itemList").innerHTML = list;  // list 내용 표시
 			
 			var remove = document.querySelectorAll(".close");  // 삭제 버튼을 변수로 저장. 배열 형태가 됨
 			for(var i = 0; i < remove.length; i++) {  // remove 배열의 요소 모두를 확인
