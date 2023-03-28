18451d6b3348f4af03aee97020b68d0a08898e02
HwangYeaEun <joyjacob1017@gmail.com>
2023-03-28 22:46:57 +0900

    챕터8까지 주석 완료

--- joyHwang/06-object/quiz-2/sol-2.js
+++ joyHwang/06-object/quiz-2/sol-2.js
@@ -5,9 +5,13 @@ document.write("반지름이 " + r + "cm일 때" + "<br>원의 둘레 : 약 " + 
 function Circle(radius) {
     this.radius = radius,
     this.circumference = function() {
-        return Math.floor(this.radius * 2 * Math.PI);
+        return Math.floor(this.radius * 2 * Math.PI); 
+        //원의 둘레=반지름 x 2 x 3.14   
+        //PI = 원주율, 파이
     }
     this.area = function() {
-        return Math.floor(Math.pow(this.radius, 2) * Math.PI);
+        return Math.floor(Math.pow(this.radius, 2) * Math.PI); 
+        //원의 넓이= 반지름 x 반지름 x 3.14  
+        //Math.pow: 제곱해주는 내장함수 (반지름의 2제곱)
     }
 }
--- joyHwang/08-dom/quiz-1/sol-1.js
+++ joyHwang/08-dom/quiz-1/sol-1.js
@@ -1,5 +1,5 @@
 var myText = document.querySelector("#myText");
-myText.addEventListener("click", function() {
+myText.addEventListener("click", function() { //클릭하면 다음과 같은 css 먹도록
     myText.style.fontSize = "20px";
     myText.style.color = "blue";
     myText.style.backgroundColor = "#ccc";
