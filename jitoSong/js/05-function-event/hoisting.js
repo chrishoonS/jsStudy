99c3417fc06079adee3635ba37aef598e0402e74
thdwlgh44 <thdwlgh44@naver.com>
2023-04-01 09:35:22 +0900

    23.04.01 챕터별 주석 달기 완료

--- jitoSong/js/05-function-event/hoisting.js
+++ jitoSong/js/05-function-event/hoisting.js
@@ -1,8 +1,8 @@
-var x = 100;
+var x = 100; //x 변수에 100 값 대입
 
-test();
+test(); //test라는 함수 호출
 
 function test() { 			
-	document.write("x is " + x + ", y is " + y);
-  var y = 200;
+	document.write("x is " + x + ", y is " + y); //출력이 먼저되어서
+  var y = 200;  //뒤에 선언된 y는 undefined
 }
--- jitoSong/js/06-object/quiz-1.js
+++ jitoSong/js/06-object/quiz-1.js
@@ -1,11 +1,11 @@
-var member1 = {
+var member1 = { //id, name, age, address를 key: value 형태로 가지고 있는 객체 선언
     id   	: 123, 
     name 	: "홍길동",
     age  	: 25,
     address : "서울"
 };
 
-document.write("<h2>" + member1.name + "</h2>");
+document.write("<h2>" + member1.name + "</h2>"); //객체.속성명으로 값을 불러옴
 document.write("<ul><li>id : " + member1.id + "</li>");
 document.write("<li>나이 : " + member1.age + "</li>");
 document.write("<li>주소 : " + member1.address + "</li></ul>");	
