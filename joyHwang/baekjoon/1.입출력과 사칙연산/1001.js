bc19e99c0724d26425227fe235ef315a1b8a4e82
HwangYeaEun <joyjacob1017@gmail.com>
2023-05-30 23:28:24 +0900

    step5까지 완료

---
+++ joyHwang/baekjoon/1.입출력과 사칙연산/1001.js
@@ -0,0 +1,7 @@
+// 1001
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+let input = fs.readFileSync('/Users/황예은/Documents/GitHub/jsStudy/joyHwang/baekjoon/1001.js').toString().split(' ');
+let a = parseInt(input[0]);
+let b = parseInt(input[1]);
+console.log(a-b);
---
+++ joyHwang/baekjoon/1.입출력과 사칙연산/1008.js
@@ -0,0 +1,7 @@
+// 	1008
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+let input = fs.readFileSync('/Users/황예은/Documents/GitHub/jsStudy/joyHwang/baekjoon/test.txt').toString().split(' ');
+let a = parseInt(input[0]);
+let b = parseInt(input[1]);
+console.log(a/b);
