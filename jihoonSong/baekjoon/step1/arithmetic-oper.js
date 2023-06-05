8649a43dbfbeac9d6bc1fcb49f45643d96973a71
jihoon.song <jihoon.song@medisolution.co.kr>
2023-06-05 13:52:28 +0900

    1단계-입출력과 사칙연산

--- jihoonSong/baekjoon/step1/arithmetic-oper.js
+++ jihoonSong/baekjoon/step1/arithmetic-oper.js
@@ -1,10 +1,78 @@
 // 2557
-console.log("Hello World!")
+// console.log("Hello World!")
 
-// 1000
-let fs = require('fs');
-// let input = fs.readFileSync('/dev/stdin').toString().split(' ');
-let input = fs.readFileSync('/Users/song/Desktop/jsStudy/jihoonSong/baekjoon/test.txt').toString().split(' ');
-let a = parseInt(input[0]);
-let b = parseInt(input[1]);
+// 1000, 1001, 10998, 1008, 10869
+/*
+var fs = require('fs');
+// var input = fs.readFileSync('/dev/stdin').toString().split(' ');
+// var i = require('fs').readFileSync('/dev/stdin').toString().split(' ').map(function(e) { return parseInt(e); });
+var input = fs.readFileSync('/Users/song/Desktop/jsStudy/jihoonSong/baekjoon/test.txt').toString().split(' ');
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
 console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+*/
+
+/*
+// 10926
+var fs = require('fs');
+// var input = fs.readFileSync('/dev/stdin').toString().split(' ');
+var input = fs.readFileSync('/Users/song/Desktop/jsStudy/jihoonSong/baekjoon/test.txt').toString().split(' ');
+console.log(input[0] + "??!");
+*/
+
+
+/*
+// 10926
+var fs = require('fs');
+var input = fs.readFileSync('/Users/song/Desktop/jsStudy/jihoonSong/baekjoon/test.txt').toString().split(' ');
+console.log(input[0] + "??!");
+*/
+
+/*
+// 18108
+var fs = require('fs');
+var input = fs.readFileSync('/Users/song/Desktop/jsStudy/jihoonSong/baekjoon/test.txt').toString().split(' ');
+console.log(parseInt(input[0]) - 543);
+*/
+
+/*
+//10430
+var fs = require('fs');
+var input = fs.readFileSync('/Users/song/Desktop/jsStudy/jihoonSong/baekjoon/test.txt').toString().split(' ');
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+var c = parseInt(input[2]);
+console.log((a+b)%c);
+console.log(((a%c)+(b%c))%c);
+console.log((a*b)%c);
+console.log(((a%c)*(b%c))%c);
+*/
+
+/*
+// 11382
+var fs = require('fs');
+var input = fs.readFileSync('/Users/song/Desktop/jsStudy/jihoonSong/baekjoon/test.txt').toString().split(' ');
+console.log(parseInt(input[0]) - 543);
+*/
+
+
+/*
+// 10171
+console.log("\\    /\\\n"+
+            " )  ( \')\n"+
+            "(  /  )\n"+
+            " \\(__)|\n")
+*/
+
+/*
+// 10172
+console.log("|\\_/|\n" +
+            "|q p|   /}\n" +
+            "( 0 )\"\"\"\\\n" +
+            "|\"^\"`    |\n" +
+            "||_/=\\\\__|");
+*/
