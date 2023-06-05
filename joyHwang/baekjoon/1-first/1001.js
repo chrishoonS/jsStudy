f0931588102bbe81f29c512b97b02a2724e2b3c4
Yeaeun Hwang <100745112+Yeaeunhwang@users.noreply.github.com>
2023-06-05 16:31:43 +0900

    wip

--- joyHwang/baekjoon/1-first/1001.js
+++ joyHwang/baekjoon/1-first/1001.js
@@ -1,4 +1,3 @@
-// 1001
 let fs = require('fs');
 // let input = fs.readFileSync('/dev/stdin').toString().split(' ');
 let input = fs.readFileSync('joyHwang/baekjoon/1-first/test.txt').toString().split(' ');
--- joyHwang/baekjoon/1-first/10998.js
+++ joyHwang/baekjoon/1-first/10998.js
@@ -1,7 +1,6 @@
-// 	10998
 let fs = require('fs');
 // let input = fs.readFileSync('/dev/stdin').toString().split(' ');
 let input = fs.readFileSync('joyHwang/baekjoon/1-first/test.txt').toString().split(' ');
 let a = parseInt(input[0]);
 let b = parseInt(input[1]);
-console.log(a*b);
+console.log(a*b, a-b, a*b, a/b);
