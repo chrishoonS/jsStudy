917b66aa05860578910733c1c60f1b4e6d4a067f
Yeaeun Hwang <100745112+Yeaeunhwang@users.noreply.github.com>
2023-06-14 18:16:30 +0900

    wip2

--- joyHwang/baekjoon/step02/14681.js
+++ joyHwang/baekjoon/step02/14681.js
@@ -1,6 +1,6 @@
 let fs = require('fs');
 // let input = fs.readFileSync('/dev/stdin').toString().split('/n');
-let input = fs.readFileSync('joyHwang/baekjoon/step02/test.txt').toString().split('/n');
+let input = fs.readFileSync('joyHwang/baekjoon/step02/test.txt').toString().split('\n');
 
 let x= parseInt(input[0]);
 let y= parseInt(input[1]);
@@ -16,4 +16,13 @@ if (x > 0 && y > 0) {
 }
 
 
+/* const [x, y] = input;
 
+if ( x > 0 ) 
+    console.log( y > 0 ? 1 : 4);
+else 
+    console.log( y < 0 ? 2 : 3) */
+
+
+
+// ? : 조건부 연산자. (피연산자가 세 개이기 때문에 '삼항(ternary)연산자라고도 함.)
