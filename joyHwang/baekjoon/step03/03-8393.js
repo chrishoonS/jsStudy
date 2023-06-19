8a458736ad4fe24f25efcdfaa098a62cbfacc63c
HwangYeaEun <joyjacob1017@gmail.com>
2023-06-19 23:48:31 +0900

    8393번 수정

--- joyHwang/baekjoon/step03/03-8393.js
+++ joyHwang/baekjoon/step03/03-8393.js
@@ -1,11 +1,10 @@
 let fs = require('fs');
 // let input = fs.readFileSync('/dev/stdin').toString();
 let input = fs.readFileSync('joyHwang/baekjoon/step03/test.txt').toString();
-
 let sum = 0;
-for (var i = 1; i <= Number(input[0]); i++) {
+for (var i = 1; i <= Number(input); i++) {
+    console.log(typeof Number(input))
     sum += i;
 } 
 console.log(sum);
-
 
