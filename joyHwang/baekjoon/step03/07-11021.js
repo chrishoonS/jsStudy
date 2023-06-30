b933fc519c77fc6e22cc3adafb174b546868401c
Yeaeun Hwang <100745112+Yeaeunhwang@users.noreply.github.com>
2023-06-30 15:06:47 +0900

    11021완성

---
+++ joyHwang/baekjoon/step03/07-11021.js
@@ -0,0 +1,10 @@
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+let input = fs.readFileSync('joyHwang/baekjoon/step03/test.txt').toString().split('\n');
+
+const T = Number(input[0]);
+
+for(var i = 1; i <= T; i++) { 
+    let numbers = input[i].split(' ');
+    console.log("Case #" + String(i) + ':' + (Number(numbers[0]) + Number(numbers[1]))); 
+}
