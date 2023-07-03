6aecb23b887e8a1710df7a0ddd91ec5f47eef19b
thdwlgh44 <thdwlgh44@naver.com>
2023-07-03 22:19:34 +0900

    230703 gugudan

--- jihoonSong/baekjoon/step03/01-2739.js
+++ jihoonSong/baekjoon/step03/01-2739.js
@@ -1,9 +1,17 @@
 // 구구단
 
-let fs = require('fs');
+// let fs = require('fs');
 // var input = fs.readFileSync('/dev/stdin').toString().split(' ');
 // const [A, B] = require('fs').readFileSync('/dev/stdin').toString().split(' ').map(n => parseInt(n))
 
-let input = fs.readFileSync('/Users/song/Desktop/jsStudy/jihoonSong/baekjoon/test.txt').toString();
-for(var i=1; i<=9; i++)
-console.log(+input, '*', i, '=', +input*i);
+// let input = fs.readFileSync('/Users/song/Desktop/jsStudy/jihoonSong/baekjoon/test.txt').toString();
+// for(var i=1; i<=9; i++)
+// console.log(+input, '*', i, '=', +input*i);
+
+
+    for (let dan=2; dan<10; dan++) { //dan: 2,3,4,5,6,7,8,9
+        for (let gob=1; gob<10; gob++) { //곱하는 수: 1,2,3,4,5,6,7,8,9
+            console.log(`${dan} * ${gob} = ${dan * gob}`.trim())
+        }
+    }
+
---
+++ jitoSong/backjoon/step3/2739_adv.js
@@ -0,0 +1,6 @@
+
+    for (let dan=2; dan<10; dan++) { //dan: 2,3,4,5,6,7,8,9
+        for (let gob=1; gob<10; gob++) { //곱하는 수: 1,2,3,4,5,6,7,8,9
+            console.log(`${dan} * ${gob} = ${dan * gob}`)
+        }
+    }
