ffb883f94c8d922cf356b75d7b33e75a36373856
Yeaeun Hwang <100745112+Yeaeunhwang@users.noreply.github.com>
2023-07-06 17:55:21 +0900

    10807 wip

---
+++ joyHwang/baekjoon/step04/01-10807.js
@@ -0,0 +1,16 @@
+//개수 세기
+
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString();
+let input = fs.readFileSync('joyHwang/baekjoon/step04/test.txt').toString().split('\n');
+
+//정수의 개수
+//정수가 공백으로 구분
+//찾으려는 정수
+
+let number = parseInt(input[0]);
+let v = parseInt(input[2])
+console.log(v)
+
+
+
