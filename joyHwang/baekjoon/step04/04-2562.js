95829d9ee595259aa0d2d35fe7f6c124b76a2080
Yeaeun Hwang <100745112+Yeaeunhwang@users.noreply.github.com>
2023-07-10 17:56:55 +0900

    10813푸는 중

---
+++ joyHwang/baekjoon/step04/04-2562.js
@@ -0,0 +1,38 @@
+//최댓값
+
+/* 3
+29
+38
+12
+57
+74
+40
+85
+61 */
+
+/* 85
+8 */
+
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+let input = fs.readFileSync('joyHwang/baekjoon/step04/test.txt').toString().split('\n').map(Number); //[3, 29, 3, 12, 57, 74, 40, 85, 61]
+
+
+
+//자연수의 총 개수
+//최댓값
+//몇 번째 인지
+let maxNum = input[0]; //최댓값을 0번째 인덱스로 변수에 담아두기
+let maxIdx = 0;
+
+for(let i = 0; i < input.length; i++) {  
+        if(maxNum < input[i]) { //i번째 인덱스 값이 앞에 저장된 최댓값보다 크면
+        maxNum = input[i]; //그 값을 최댓값으로 저장
+        maxIdx = i + 1;
+        // maxIdx = i;
+    }
+}
+console.log(maxNum)
+// // console.log(maxIdx + 1)
+console.log(maxIdx)
+
---
+++ joyHwang/baekjoon/step04/06-10813.js
@@ -0,0 +1,13 @@
+//공 바꾸기
+
+/* 5 4
+1 2
+3 4
+1 4
+2 2 */
+
+/* 3 1 4 2 5*/
+
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString();
+let input = fs.readFileSync('joyHwang/baekjoon/step04/test.txt').toString().split('\n'); 
