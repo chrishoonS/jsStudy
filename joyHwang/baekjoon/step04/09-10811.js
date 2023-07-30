7bc4bd3f4f46e430db49dd95ca16379fc7ece453
HwangYeaEun <joyjacob1017@gmail.com>
2023-07-30 10:36:38 +0900

    wip 10811

--- joyHwang/baekjoon/step04/09-10811.js
+++ joyHwang/baekjoon/step04/09-10811.js
@@ -17,5 +17,44 @@ let fs = require('fs');
 let input = fs.readFileSync('joyHwang/baekjoon/step04/test.txt').toString().split('\n');
 
 const [n, m] = input[0].split(" ").map(Number) //[5, 4]
-//console.log([n, m])
+// console.log([n, m])
+const basket = []
 
+for (let i = 1; i <=n; i++) {
+    basket.push(i);
+}
+//console.log(basket)
+
+for(let i = 1; i <= m; i++) {
+    //console.log(i)
+    let [a, b] = input[i].split(' ').map(Number);
+    //console.log([a, b]) [ 1, 2 ] [ 3, 4 ] [ 1, 4 ] [ 2, 2 ]
+    let tmpArr = [];
+    for(let j = a-1; j < b; j++) { //왜 -1? index를 의미하나? 
+        //console.log(j) 0 1 / 2 3 / 0 1 2 3 / 1
+        tmpArr.push(basket[j]); 
+        //console.log(basket)  [ 1, 2, 3, 4, 5 ] [ 1, 2, 3, 4, 5 ] [ 2, 1, 3, 4, 5 ] [ 2, 1, 3, 4, 5 ] [ 2, 1, 4, 3, 5 ] [ 2, 1, 4, 3, 5 ] [ 2, 1, 4, 3, 5 ] [ 2, 1, 4, 3, 5 ] [ 3, 4, 1, 2, 5 ]
+    };
+    tmpArr.reverse();
+    console.log(tmpArr)
+    basket.splice(a-1, b-a+1, ...tmpArr);
+}
+console.log(basket.join(' '))
+
+
+/* spread 연산자 ... */
+
+
+/* 
+reverse() 함수는 배열의 순서를 거꾸로 만들어 줍니다
+이 함수는 이 함수를 호출한 배열을 거꾸로 뒤집고, 그 배열을 가리키는 참조값을 반환합니다.
+
+따라서, 이 함수를 실행시키면 원본 배열이 변형됩니다.
+
+https://hianna.tistory.com/448
+*/
+
+/* 
+splice()
+배열의 기존 요소를 삭제 또는 교체하거나 새 요소를 추가하여 배열의 내용을 변경합니다.
+*/
