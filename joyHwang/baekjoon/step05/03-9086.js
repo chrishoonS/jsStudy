42b7cf553531ee263317f9266495ad47fae7a7e7
HwangYeaEun <joyjacob1017@gmail.com>
2023-08-07 22:58:47 +0900

    11654 완료

--- joyHwang/baekjoon/step05/03-9086.js
+++ joyHwang/baekjoon/step05/03-9086.js
@@ -21,6 +21,14 @@ let input = fs.readFileSync('joyHwang/baekjoon/step05/test.txt').toString().trim
 const num = parseInt(input[0]); //3
 
 for(let i = 1; i <= num; i++) { //1번째 인덱스부터 3번 반복
-    let first = input[i];
-    let last = input[input.lengh];
+    let first = input[i].trim(); //ACDKJFOWIEGHE O AB
+    let last = first.length - 1; // 12 0 1
+    console.log(
+        input[i].substring(0, 1) + input[i].substring(last, input[i].length)
+    )
 }
+
+//각 입력값의 0~1번째 index, 즉 첫번째 문자와  
+
+
+//substring() 메소드는 string 객체의 시작 인덱스로 부터 종료 인덱스 전 까지 문자열의 부분 문자열을 반환합니다. 
---
+++ joyHwang/baekjoon/step05/04-11654.js
@@ -0,0 +1,14 @@
+//아스키 코드
+
+/* 
+A
+*/
+
+/* 
+65
+*/
+
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('');
+let input = fs.readFileSync('joyHwang/baekjoon/step05/test.txt').toString().trim().split('\n'); 
+
