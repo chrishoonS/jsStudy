dfd9549c458b07818cb73aef22e14a61848c2308
HwangYeaEun <joyjacob1017@gmail.com>
2023-09-12 23:33:36 +0900

    10809까지 완료

--- joyHwang/baekjoon/step05/04-11654.js
+++ joyHwang/baekjoon/step05/04-11654.js
@@ -10,5 +10,9 @@ A
 
 let fs = require('fs');
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split('');
-let input = fs.readFileSync('joyHwang/baekjoon/step05/test.txt').toString().trim().split('\n'); 
+let input = fs.readFileSync('joyHwang/baekjoon/step05/test.txt').toString().trim();
 
+console.log(input.charCodeAt(0));
+
+//charCodeAt: 자바스크립트에서 문자를 아스키코드로 변환하는 메서드
+//"문자열".charCodeAt([문자열 자릿수]);
---
+++ joyHwang/baekjoon/step05/06-10809.js
@@ -0,0 +1,21 @@
+//알파벳 찾기
+
+/* 
+baekjoon
+*/
+
+/* 
+1 0 -1 -1 2 -1 -1 -1 -1 4 3 -1 -1 7 5 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1
+*/
+
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('');
+let input = fs.readFileSync('joyHwang/baekjoon/step05/test.txt').toString().trim().split('');
+const result = [];
+for (i = 97; i <= 122; i++) {//알파벳만큼 반복 a=97, z=122
+    result.push(input.indexOf(String.fromCharCode(i)));
+}
+console.log(result.join(' '));
+
+//indexOf: 값이 있다면 그 값의 index를, 없다면 -1을 출력하는 메서드! (복습)
+//String.fromCharCode: 숫자(i)에서 알파벳 문자를 생성하는 메서드 -- 더 찾아보기
