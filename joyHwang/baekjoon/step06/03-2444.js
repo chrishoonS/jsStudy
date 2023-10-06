b94788e220090767805b0f7195cb273c3371af72
Yeaeun Hwang <100745112+Yeaeunhwang@users.noreply.github.com>
2023-10-06 16:54:10 +0900

    2444 wip

---
+++ joyHwang/baekjoon/step06/03-2444.js
@@ -0,0 +1,29 @@
+//별 찍기 - 7
+
+/* 
+5
+*/
+
+/* 
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+*/
+
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString();
+let input = fs.readFileSync('joyHwang/baekjoon/step06/test.txt').toString().map(Number); 
+const result = '';
+
+//위에 다섯개
+for(let i = 0; i < input.length; i++) {
+    
+}
+//https://luminouswy.tistory.com/entry/%EB%B0%B1%EC%A4%80-2444%EB%B2%88-%EB%B3%84%EC%B0%8D%EA%B8%B0-javascript-nodejs
+//https://aiday.tistory.com/114
