9f2866f9d473f6d37bff95c6564d0b6280276f8f
Yeaeun Hwang <100745112+Yeaeunhwang@users.noreply.github.com>
2023-10-11 17:55:35 +0900

    wip

--- joyHwang/baekjoon/step06/03-2444.js
+++ joyHwang/baekjoon/step06/03-2444.js
@@ -18,12 +18,28 @@
 
 let fs = require('fs');
 // let input = fs.readFileSync('/dev/stdin').toString();
-let input = fs.readFileSync('joyHwang/baekjoon/step06/test.txt').toString().map(Number); 
-const result = '';
+let input = fs.readFileSync('joyHwang/baekjoon/step06/test.txt').toString(); 
+const n = parseInt(input);
+const result = [];
 
 //위에 다섯개
-for(let i = 0; i < input.length; i++) {
-    
+for(let i = 1; i <= n; i++) { 
+    // console.log(i) //12345
+    for(let j = 1; j < n+i; j++) {//5+1, 5+2, 5+3, 5+4, 5+5
+        console.log(j) //12345123456123456712345678123456789
+        if(j <= n-i) {//5-1, 5-2, 5-3, 5-4, 5-5
+            result += ' ';
+        } else {
+            result += '*';
+        }
+    }
+    result += '\n';
 }
+console.log(result)
+//아래 네개
+
+
+
+
 //https://luminouswy.tistory.com/entry/%EB%B0%B1%EC%A4%80-2444%EB%B2%88-%EB%B3%84%EC%B0%8D%EA%B8%B0-javascript-nodejs
 //https://aiday.tistory.com/114
