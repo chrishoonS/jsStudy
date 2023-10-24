c0671de42147ad5b3e547c7fc0362b6829627740
송지훈 <jihoon.song@medisolution.co.kr>
2023-10-24 10:27:39 +0900

    코드 리팩토링

--- jihoonSong/baekjoon/step06/04-10988.js
+++ jihoonSong/baekjoon/step06/04-10988.js
@@ -3,11 +3,8 @@ const input = require('fs').readFileSync('/Users/song/Desktop/jsStudy/jihoonSong
 let result = 1;
 for(let i=0; i<input.length; i++){
     input[i] = input[input.length-i-1]
-    if(input[i] == input[input.length-i-1]){
-        result = 1;
-    }else {
-        result = 0;
-    }
+
+    result = ( input[i] == input[input.length-i-1] ) ? 1 : 0;
 }
 
 console.log(result);
