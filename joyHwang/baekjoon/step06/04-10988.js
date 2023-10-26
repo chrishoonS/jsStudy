89c57e451f9bc94a799a246eaeacd149affde9bf
HwangYeaEun <joyjacob1017@gmail.com>
2023-10-27 00:07:38 +0900

    1027스터디 완료

--- joyHwang/baekjoon/step06/04-10988.js
+++ joyHwang/baekjoon/step06/04-10988.js
@@ -9,8 +9,9 @@ level
 
 let fs = require('fs');
 // let input = fs.readFileSync('/dev/stdin').toString().split(''); 
-let input = fs.readFileSync('joyHwang/baekjoon/step06/test.txt').toString().split('').trim(); 
+let input = fs.readFileSync('joyHwang/baekjoon/step06/test.txt').toString().split(''); 
 
-const first = input[0];
-const last = input.reverse()[0];
-first === last ? console.log('1') : console.log('0')
+//반복문! 으로 다시!
+// const first = input[0];
+// const last = input.reverse()[0];
+first === last ? console.log(1) : console.log(0)
---
+++ joyHwang/baekjoon/step06/05-1157.js
@@ -0,0 +1,15 @@
+//단어 공부
+/* 
+Mississipi zZa z baaa
+*/
+
+/* 
+? Z Z A
+*/
+
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split(''); 
+let input = fs.readFileSync('joyHwang/baekjoon/step06/test.txt').toString().split('').trim(); 
+
+// https://velog.io/@daekuenhan/%EB%B0%B1%EC%A4%80node.js-1157%EB%B2%88-%EB%8B%A8%EC%96%B4-%EA%B3%B5%EB%B6%80
+// https://itsseo.tistory.com/entry/%EB%B0%B1%EC%A4%80-%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4-javascript-%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8-1157%EB%B2%88-%EB%8B%A8%EC%96%B4-%EA%B3%B5%EB%B6%80
