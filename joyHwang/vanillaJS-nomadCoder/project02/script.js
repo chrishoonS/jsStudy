18ba70810e8b293fe2d243f7c73b46a673cfc5ba
Yeaeun Hwang <100745112+Yeaeunhwang@users.noreply.github.com>
2023-12-04 15:07:55 +0900

    강의 4.6시청완료

--- joyHwang/vanillaJS-nomadCoder/project02/script.js
+++ joyHwang/vanillaJS-nomadCoder/project02/script.js
@@ -1,16 +1,16 @@
-const windowWidthWide = 900;
-const windowWidthSmall = 600;
-const body = document.querySelector("body");
+const windowWidthWide = 700;
+const windowWidthSmall = 500;
+const body = document.body;
 
 function changeBackgroundColor() {
   const windowWidth = window.innerWidth;
 
   if (window.innerWidth > windowWidthWide) {
-    body.classList.add("bg-yellow");
     body.classList.remove("bg-purple");
+    body.classList.add("bg-yellow");
   } else if (
-    window.innerWidth < windowWidthWide &&
-    windowWidth > windowWidthSmall
+    window.innerWidth <= windowWidthWide &&
+    windowWidth >= windowWidthSmall
   ) {
     body.classList.remove("bg-yellow", "bg-blue");
     body.classList.add("bg-purple");
