06b48d7d0552c98481ae8feb9d63b9a04fe67c17
HwangYeaEun <joyjacob1017@gmail.com>
2024-01-07 10:49:41 +0900

    주석 넣기

--- joyHwang/programmers/Lv.0/fraction.js
+++ joyHwang/programmers/Lv.0/fraction.js
@@ -4,13 +4,41 @@
 //1	2	3	4	[5, 4]
 //9	2	1	3	[29, 6]
 
+
+//방법 1
 function solution(numer1, denom1, numer2, denom2) {
-    const sumdenom = denom1 * denom2;
-    const sumnumer = numer1 * denom2 + numer2 * denom1;
-    const gcd = findGcd(sumdenom, sumnumer);
-    return [ sumnumer /gcd, sumdenom /gcd];
+    //분자 구하기 (??왜)
+    let denum = denum1 * num2 + denum2 * num1;
+    //분모 구하기
+    let num = num1 * num2;
+    let a = denum;
+    let b = num;
 }
-function findGcd(a,b){
-    return b === 0 ? a: findGcd(b, a % b);
+
+//최대공약수 구하기
+// 1. a, b를 서로 나눈다. 만약 나누어진다면 b가 최대 공약수이다. (a > b)
+// 2. 서로가 나누어지지 않는다면 b 와 a % b (a를 b로 나눈 나머지)로 다시 나눈다.
+// 3. 서로가 나누어진다면 a % b 가 최대공약수이다. 만약 나누어지지 않는다면 다시 위 방법을 반복한다.
+const gcd = (a, b) => (a % b === 0 ? b : gcd(b, a % b));
+
+//최대공약수를 분자 분모에 나누고 배열에 넣기
+answer[0] = a / gcd(a,b);
+answer[1] = b / gcd(a,b);
+
+return answer;
+
+//방법 2
+function cal_gcd(a, b) {
+    return a % b === 0 ? b : cal_gcd(b, a % b)
+}
+
+function solution(denum1, num1, denum2, num2) {
+    let denum = denum1 * num2 + denum2 * num1;
+    let num = num1 * num2;
+    let gcd = cal_gcd(denum, num);
+    
+    // 최대 공약수를 분자 분모에 나누고 배열에 넣기
+    return [denum / gcd, num / gcd]
 }
 //https://luck131313.tistory.com/76
+//https://velog.io/@gagaeun/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EB%B6%84%EC%88%98%EC%9D%98-%EB%8D%A7%EC%85%88
