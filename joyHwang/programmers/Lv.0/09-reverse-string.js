707c638455a1cb3f644c4a09d6b8dd031c999e0b
Yeaeun Hwang <100745112+Yeaeunhwang@users.noreply.github.com>
2024-03-15 17:59:29 +0900

    문자열 뒤집기 풀이

---
+++ joyHwang/programmers/Lv.0/09-reverse-string.js
@@ -0,0 +1,13 @@
+// 문자열 my_string이 매개변수로 주어집니다. my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요.
+
+function solution(my_string) {
+    let reverseText = my_string.toString().split(''); //typeof : object
+    let result = reverseText.reverse().join(''); //typeof : string
+    // let result = reverseText.reverse();
+    return result;
+    
+}
+
+console.log(solution("bread"))
+
+//join()을 사용해서 split 되어있던 배열 객체를 string으로 바꿔줘야 정답!
