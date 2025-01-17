2e3a03a4bdf3bbcc2de7abc4028ef1009eed926a
송지훈 <jihoon.song@medisolution.co.kr>
2025-01-17 16:36:17 +0900

    long polling study

---
+++ jihoonSong/backend/04-long-polling.js
@@ -0,0 +1,39 @@
+// 터미널에서 curl -X POST http://localhost:8080으로 호출
+// curl http://localhost:8080/checkstatus?jobId=job:1737016888837 을 통해 현재 jobStatus 확인 가능
+// 서버는 setTimeout 메서드를 통해 prg를 +10
+// 
+
+const app = require("express")();
+const jobs = {};
+
+app.post("/submit", (req, res) => {
+    const jobId = `job:${Date.now()}`;
+    jobs[jobId] = 0;
+    updateJob(jobId, 0);
+    res.end("\n\n" + jobId + "\n\n");
+});
+
+app.get("/checkstatus", async (req, res) => {
+    console.log(jobs[req.query.jobId]);
+    while(await checkJobComplete(req.query.jobId) == false);
+    res.end("\n\nJobStatus: " + jobs[req.query.jobId] + "%\n\n");
+});
+
+app.listen(8080, () => console.log("litstening on 8080"));
+
+async function checkJobComplete(jobId) {
+    return new Promise( (resolve, reject) => {
+        if(jobs[jobId] < 100)
+            this.setTimeout(() => resolve(false), 1000);
+        else
+            resolve(true);
+    });
+    
+}
+
+function updateJob(jobId, prg){
+    jobs[jobId] = prg;
+    console.log(`updated ${jobId} to ${prg}`);
+    if(prg == 100) return;
+    this.setTimeout(() => updateJob(jobId, prg+10), 3000);
+}
